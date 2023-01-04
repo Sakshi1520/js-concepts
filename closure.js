53b4de0474ac16627036af4f025927be4f69b838
Sakshi Kadamm <sakshikadam1520@gmail.com>
2023-01-04 09:20:15 +0530

    Closure challenge

--- closure.js
+++ closure.js
@@ -256,3 +256,73 @@ const makeHistory = (limit) => {
 // console.log(myActions('undo')); // should log: 'pose undone'
 // console.log(myActions('undo')); // should log: 'code undone'
 // console.log(myActions('undo')); // should log: 'nothing to undo'
+
+
+// Challenge 12
+const blackjack = (array) => {
+    let playerCalledCount = 0;
+		let firstCall = true;
+  
+	return (num1, num2) => {
+    let resultOfPlayer = 0;
+    let isBust = false;
+    firstCall = true;
+    
+    return () => {
+      if(firstCall) {
+        firstCall = false;
+      	playerCalledCount++;
+        resultOfPlayer = num1 + num2;
+        return resultOfPlayer;
+      }
+      else {
+        resultOfPlayer += array[playerCalledCount - 1];
+        if(isBust) {
+          return 'you are done!';
+        }
+        if(resultOfPlayer <= 21) {
+      		playerCalledCount++;
+          return resultOfPlayer;
+        } else {
+          isBust = true;
+          return 'bust';
+        }
+      }
+      console.log("Count", playerCalledCount);
+    }
+  }
+};
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+const i_like_to_live_dangerously = deal(4, 5);
+console.log(i_like_to_live_dangerously()); // should log: 9
+console.log(i_like_to_live_dangerously()); // should log: 11
+console.log(i_like_to_live_dangerously()); // should log: 17
+console.log(i_like_to_live_dangerously()); // should log: 18
+console.log(i_like_to_live_dangerously()); // should log: 'bust'
+console.log(i_like_to_live_dangerously()); // should log: 'you are done!'
+console.log(i_like_to_live_dangerously()); // should log: 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+const i_TOO_like_to_live_dangerously = deal(2, 2);
+console.log(i_TOO_like_to_live_dangerously()); // should log: 4
+console.log(i_TOO_like_to_live_dangerously()); // should log: 15
+console.log(i_TOO_like_to_live_dangerously()); // should log: 19
+console.log(i_TOO_like_to_live_dangerously()); // should log: 'bust'
+console.log(i_TOO_like_to_live_dangerously()); // should log: 'you are done!
+console.log(i_TOO_like_to_live_dangerously()); // should log: 'you are done!
+
+// /*** PLAYER 3 ***/
+const i_ALSO_like_to_live_dangerously = deal(3, 7);
+console.log(i_ALSO_like_to_live_dangerously()); // should log: 10
+console.log(i_ALSO_like_to_live_dangerously()); // should log: 13
+console.log(i_ALSO_like_to_live_dangerously()); // should log: 'bust'
+console.log(i_ALSO_like_to_live_dangerously()); // should log: 'you are done!
+console.log(i_ALSO_like_to_live_dangerously()); // should log: 'you are done!
