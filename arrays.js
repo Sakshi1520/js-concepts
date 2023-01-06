a676e4fc8d7acc515f071f96e9faa84d47c288a3
Sakshi Kadamm <sakshikadam1520@gmail.com>
2023-01-06 23:16:36 +0530

    Array problems

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// Double elements of an array using reduce
+// const arr = [1, 2, 3, 4];
+// arr.reduce((acc, curr, index) => {
+//   arr[index] = curr * 2;
+// }, arr[0]);
+// console.log(arr);
+
+
+// Unique values in array
+
+const unique = new Set([1, 1, 1, 1, 2]);
+// console.log([...unique]);
+
+
+// Array constructor
+// console.log(Array(5).fill(4));
